bc0735f1d476e2b9bdf89e75fb19183d4f2d87e4
Чумак Сергей Викторович <svchumak1@mts.ru>
2021-09-09 22:00:28 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'online-store';
+  protected efsfesewr23;
+  private readonly nefw;
+  public title = 'online-store';
+
+  public efewefwef = [
+    { ad: 5848 },
+    { sdwda: 58415 },
+    { dwadwadadw: 4884848 },
+    { swdawdaw: 544844 },
+  ];
+
+  public dwdwdawd;
 }
