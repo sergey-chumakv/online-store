688f53a139b3136c0401b5c6f718fcd41937a5ad
Чумак Сергей Викторович <svchumak1@mts.ru>
2021-09-12 13:15:18 +0300

    fixes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements OnInit {
-  public toggleDarkTheme = false;
-
-  public ngOnInit(): void {}
-
-  public onToggle(checked: boolean): void {
-    this.toggleDarkTheme = checked;
-  }
-}
+export class AuthComponent {}
